e74d052c836fbfdfcc0dab21ad578790f8307b43
Alexander Parkhamovich <alex.parkhamovich@gmail.com>
2021-02-09 12:33:28 +0300

    before deploy cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,16 +16,6 @@ class User < ApplicationRecord
   belongs_to :referrer, class_name: 'User', optional: true
 end
 
-# TODO:
-# DEVELOPMENT:
-# + UNIQ CODE instead of USER_ID
-# + USERS index PAGE WITH uniq code
-# + RSPEC ALL STUFF instead of AUTH services
-# + REFACTORING AUTH STUFF
-# + RSPEC AUTH STUFF
-# 6) CHANGE localohost:3000 to APPLICATION_HOST (add application.yml)
-# 7) users#index spec + responder + responder spec (localhost:3000)
-
 # DEPLOY:
 # 1) push Git
 # 2) push Heroku
