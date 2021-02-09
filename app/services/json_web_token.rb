607fa463460710b1d900d3e0aac0d3c88d3b321a
Alexander Parkhamovich <alex.parkhamovich@gmail.com>
2021-02-09 13:59:43 +0300

    heroku: secret key base fix

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -3,11 +3,11 @@ class JsonWebToken
    def encode(payload, expire_in = 24.hours.from_now)
      payload[:exp] = expire_in.to_i
 
-     JWT.encode(payload, Rails.application.secrets.secret_key_base)
+     JWT.encode(payload, ENV.fetch('SECRET_KEY_BASE'))
    end
 
    def decode(token)
-     body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+     body = JWT.decode(token, ENV.fetch('SECRET_KEY_BASE'))[0]
 
      HashWithIndifferentAccess.new(body)
    rescue
